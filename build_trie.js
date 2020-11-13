66076b1247776565590c211539ece333f7e4a33b
Diegoalesco95 <42728227+Diegoalesco95@users.noreply.github.com>
2020-11-13 09:24:53 -0500

    👌 IMPROVE: Iterate each letter of the word and return an object with the array2Object function

--- build_trie.js
+++ build_trie.js
@@ -1,11 +1,21 @@
 'use strict';
 
-//  Function de create a trie
+//  Function to pass an array to an object
+function array2Object(array, key) {
+  return array.reduce((obj, item) => ({ ...obj, [item]: item }), {});
+}
+
+//  Function to create a trie
 function buildTrie() {
+  let trie = [];
   for (let i = 0; i < arguments.length; i += 1) {
-    console.log(arguments[i]);
+    const word = arguments[i];
+    for (let j = 0; j < word.length; j += 1) {
+      trie.push(word.charAt(j));
+    }
   }
+  console.log(array2Object(trie));
 }
 
 // Execute the function
-buildTrie('ae', 'ei', 'io', 'ou');
+buildTrie('trust');
