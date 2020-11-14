67ff2c4eb1d0afd316a0c59ec39bad7587b04a00
Diegoalesco95 <42728227+Diegoalesco95@users.noreply.github.com>
2020-11-14 10:59:23 -0500

    👌 IMPROVE: Determine if there are arguments in the function

--- build_trie.js
+++ build_trie.js
@@ -7,15 +7,19 @@ function array2Object(array, key) {
 
 //  Function to create a trie
 function buildTrie() {
-  let trie = [];
-  for (let i = 0; i < arguments.length; i += 1) {
-    const word = arguments[i];
-    for (let j = 0; j < word.length; j += 1) {
-      trie.push(word.charAt(j));
+  if (!arguments[0]) {
+    return console.log({});
+  } else {
+    let trie = [];
+    for (let i = 0; i < arguments.length; i += 1) {
+      const word = arguments[i];
+      for (let j = 0; j < word.length; j += 1) {
+        trie.push(word.charAt(j));
+      }
     }
+    console.log(array2Object(trie));
   }
-  console.log(array2Object(trie));
 }
 
 // Execute the function
-buildTrie('trust');
+buildTrie('');
