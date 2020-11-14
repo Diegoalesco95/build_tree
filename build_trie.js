abbc1e72dc385802d8ed1d297dec62f78b09185d
Diegoalesco95 <42728227+Diegoalesco95@users.noreply.github.com>
2020-11-14 12:02:42 -0500

    👌 IMPROVE: Get the value of the tree path

--- build_trie.js
+++ build_trie.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // Node
-function Node(key) {
+function NodeTree(key) {
   this.key = key;
   this.parent = null;
   this.children = {};
@@ -10,7 +10,7 @@ function Node(key) {
 
 // Tree
 function Trie() {
-  this.root = new Node(null);
+  this.root = new NodeTree(null);
 }
 
 //  Method to insert the word in the node
@@ -22,7 +22,7 @@ Trie.prototype.add = function (word) {
     // check to see if character node exists.
     if (!node.children[word[i]]) {
       // if it doesn't exist, then create it.
-      node.children[word[i]] = new Node(word[i]);
+      node.children[word[i]] = new NodeTree(word[i]);
 
       // assign the parent.
       node.children[word[i]].parent = node;
@@ -39,6 +39,52 @@ Trie.prototype.add = function (word) {
   }
 };
 
+Trie.prototype.search = function (word) {
+  let node = this.root;
+  let output = [];
+
+  let keys = [];
+  let newNode;
+  let entrie = word[0];
+
+  let temp = [];
+  let trie = [];
+
+  // for every character in the word
+  for (let i = 0; i < word.length; i++) {
+    // save a copy of the letter
+    newNode = entrie;
+
+    if (node.children[word[i]]) {
+      // join with the next letter
+      entrie = entrie + word[i + 1];
+      // save in a temp array
+      temp.push(entrie);
+      // pass to the next node
+      node = node.children[word[i]];
+    } else {
+      // there's none. just return it.
+      return output;
+    }
+    // Save the temp kies in an array
+    keys.push(array2Object([entrie], newNode));
+  }
+
+  // re asign the values of the each key in order
+  for (let j = 0; j < keys.length; j++) {
+    // obtain the key
+    const key = Object.keys(keys[j]);
+    // temporarily save the value
+    const newElm = keys[j];
+    //  verify if is the last letter
+    newElm[key] = keys[j + 1] ? keys[j + 1] : null;
+    trie[j] = newElm;
+  }
+  output = trie[0];
+
+  return output;
+};
+
 //  Function to pass an array to an object
 function array2Object(array, key) {
   return array.reduce((obj, item) => ({ ...obj, [key]: item }), {});
@@ -46,20 +92,21 @@ function array2Object(array, key) {
 
 //  Function to create a trie
 function buildTrie() {
-  // Determine if there are arguments
+  // console.log(arguments[1]);
+  let trie = new Trie();
   if (!arguments[0]) {
-    return console.log({});
+    // return console.log({});
+    return {};
   } else {
-    let trie = [];
-    for (let i = 0; i < arguments.length; i += 1) {
+    for (let i = 0; i < arguments.length; i++) {
+      // Determine if are there arguments
       const word = arguments[i];
-      for (let j = 0; j < word.length; j += 1) {
-        trie.push(word.charAt(j));
-      }
+      trie.add(word);
+      // return console.log(trie.search(word));
+      return trie.search(word);
     }
-    console.log(array2Object(trie));
   }
 }
 
 // Execute the function
-buildTrie('');
+buildTrie('tree');
