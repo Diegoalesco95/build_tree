e6c6233aa8dac1919af0b1a9654b652c87db918a
Diegoalesco95 <42728227+Diegoalesco95@users.noreply.github.com>
2020-11-18 17:29:07 -0500

    👌 IMPROVE: Change of functions to class and improvement of the method to add the word

--- build_trie.js
+++ build_trie.js
@@ -1,109 +1,45 @@
 'use strict';
 
-// Node
-function NodeTree(key) {
-  this.key = key;
-  this.parent = null;
-  this.children = {};
-  this.end = false;
-}
-
-// Tree
-function Trie() {
-  this.root = new NodeTree(null);
-}
-
-//  Method to insert the word in the node
-Trie.prototype.add = function (word) {
-  let node = this.root;
-
-  // for every character in the word
-  for (let i = 0; i < word.length; i++) {
-    // check to see if character node exists.
-    if (!node.children[word[i]]) {
-      // if it doesn't exist, then create it.
-      node.children[word[i]] = new NodeTree(word[i]);
-
-      // assign the parent.
-      node.children[word[i]].parent = node;
-    }
-
-    // proceed to the next depth in the trie.
-    node = node.children[word[i]];
-
-    // finally, check to see if is the end.
-    if (i == word.length - 1) {
-      // change the end flag to true.
-      node.end = true;
-    }
+class Trie {
+  constructor() {
+    this.root = {};
   }
-};
 
-Trie.prototype.search = function (word) {
-  let node = this.root;
-  let output = [];
-
-  let keys = [];
-  let newNode;
-  let entrie = word[0];
-
-  let temp = [];
-  let trie = [];
-
-  // for every character in the word
-  for (let i = 0; i < word.length; i++) {
-    // save a copy of the letter
-    newNode = entrie;
-
-    if (node.children[word[i]]) {
-      // join with the next letter
-      entrie = entrie + word[i + 1];
-      // save in a temp array
-      temp.push(entrie);
-      // pass to the next node
-      node = node.children[word[i]];
+  //  Method to insert a word in the node
+  add(node, word, index) {
+    // Verify if is the end of the word
+    if (index === word.length - 1) {
+      // return null as value of the last node
+      return (node[word.slice(0, index + 1)] = null);
     } else {
-      // there's none. just return it.
-      return output;
+      // Verify if the node not contain a value
+      if (!node[word.slice(0, index + 1)]) {
+        // if there is no value, assign one.
+        node[word.slice(0, index + 1)] = {};
+      }
+      // Run recursively until reaching the end of the word
+      this.add(node[word.slice(0, index + 1)], word, index + 1);
     }
-    // Save the temp kies in an array
-    keys.push(array2Object([entrie], newNode));
+    console.log(node);
+    return node[word.slice(0, index + 1)];
   }
-
-  // re asign the values of the each key in order
-  for (let j = 0; j < keys.length; j++) {
-    // obtain the key
-    const key = Object.keys(keys[j]);
-    // temporarily save the value
-    const newElm = keys[j];
-    //  verify if is the last letter
-    newElm[key] = keys[j + 1] ? keys[j + 1] : null;
-    trie[j] = newElm;
-  }
-  output = trie[0];
-
-  return output;
-};
-
-//  Function to pass an array to an object
-function array2Object(array, key) {
-  return array.reduce((obj, item) => ({ ...obj, [key]: item }), {});
 }
 
 //  Function to create a trie
 function buildTrie() {
-  // console.log(arguments[1]);
-  let trie = new Trie();
+  const trie = new Trie();
+  let node = trie.root;
+
+  // Determine if are there arguments
   if (!arguments[0]) {
-    // return console.log({});
+    console.log({});
     return {};
   } else {
     for (let i = 0; i < arguments.length; i++) {
-      // Determine if are there arguments
       const word = arguments[i];
-      trie.add(word);
-      // return console.log(trie.search(word));
-      return trie.search(word);
+
+      // Add wach word to the trie
+      trie.add(node, word, 0);
     }
   }
 }
