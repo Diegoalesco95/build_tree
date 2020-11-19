76ece0ea685ceba25da5402cc9e8796273fa26d2
Diegoalesco95 <42728227+Diegoalesco95@users.noreply.github.com>
2020-11-18 19:12:18 -0500

    👌 IMPROVE: Improved method to validate if there is an argument

--- build_trie.js
+++ build_trie.js
@@ -7,26 +7,30 @@ class Trie {
 
   //  Method to insert a word in the node
   add(node, word, index) {
-    // Verify if is the end of the word
-    if (index === word.length - 1) {
-      // return null as value of the last node
-      return (node[word.slice(0, index + 1)] = null);
-    } else {
-      // Verify if the node not contain a value
-      if (!node[word.slice(0, index + 1)]) {
-        // if there is no value, assign one.
-        node[word.slice(0, index + 1)] = {};
+    if (word) {
+      // Verify if is the end of the word
+      if (index === word.length - 1) {
+        // return null as value of the last node
+        return (node[word.slice(0, index + 1)] = null);
+      } else {
+        // Verify if the node not contain a value
+        if (!node[word.slice(0, index + 1)]) {
+          // if there is no value, assign one.
+          node[word.slice(0, index + 1)] = {};
+        }
+        // Run recursively until reaching the end of the word
+        this.add(node[word.slice(0, index + 1)], word, index + 1);
       }
-      // Run recursively until reaching the end of the word
-      this.add(node[word.slice(0, index + 1)], word, index + 1);
+      return node[word.slice(0, index + 1)];
+    } else {
+      return {};
     }
-    return node[word.slice(0, index + 1)];
   }
 
   // Method to search all words in the tree
   searchAll() {
     let node = this.root;
-    // console.log(node);
+    console.log(node);
     return node;
   }
 }
@@ -37,16 +41,9 @@ function buildTrie() {
   let node = trie.root;
 
   // Determine if are there arguments
-  if (!arguments[0]) {
-    // console.log({});
-    return {};
-  } else {
-    for (let i = 0; i < arguments.length; i++) {
-      const word = arguments[i];
-
-      // Add wach word to the trie
-      trie.add(node, word, 0);
-    }
+  for (let argument of arguments) {
+    // Add wach word to the trie
+    trie.add(node, argument, 0);
   }
   return trie.searchAll();
 }
@@ -58,3 +55,4 @@ function buildTrie() {
 // buildTrie('tree');
 // buildTrie('A', 'to', 'tea', 'ted', 'ten', 'i', 'in', 'inn');
 // buildTrie('true', 'trust');
+// buildTrie('true', 'trUe', 'truE', 'True');
